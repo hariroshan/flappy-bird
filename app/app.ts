60a1142e5a723443164504bdfb622d1d21d0d6fb
hariroshan <hariroshanmail@gmail.com>
2023-06-03 16:02:21 +0530

    inital setup of nativescript canvas and phaser

--- app/app.ts
+++ app/app.ts
@@ -6,10 +6,31 @@ purpose of the file is to pass control to the app’s first module.
 
 import Elm from "./src/Main.elm";
 import { start } from "elm-native-js"
+import { Canvas } from '@nativescript/canvas'
+import * as TNSPhaser from "@nativescript/canvas-phaser";
+import * as TaskPort from 'elm-taskport';
+import { kebabCased, view } from "elm-native-js/src/Native/Constants.bs"
+import { buildHandler, addViewRender } from "elm-native-js/src/Native/Elements.bs"
+
+let game: any;
+TaskPort.register("initialize", (args: any) => {
+  game = TNSPhaser.Game(args)
+})
+
+const canvasAttributes = [].map(kebabCased)
 
 const config = {
   elmModule: Elm,
-  elmModuleName: "Main"
+  elmModuleName: "Main",
+  customElements: [
+    { tagName: 'ns-canvas'
+    , handler: buildHandler(
+      () => new Canvas(),
+        view.concat(canvasAttributes),
+        addViewRender
+      )
+    }
+  ]
 }
 
 start(config)
--- app/elm.d.ts
+++ app/elm.d.ts
@@ -1,3 +1,19 @@
 declare module "*.elm" {
     export default () => any;
 }
+
+declare module "elm-taskport" {
+  export function register(functionName: string, fn: any) : void;
+}
+
+declare module "elm-native-js/src/Native/Elements.bs" {
+  import { buildHandler, addViewRender } from "elm-native-js/types/Native/Elements.bs"
+  export var buildHandler;
+  export var addViewRender;
+}
+
+declare module "elm-native-js/src/Native/Constants.bs" {
+  import { kebabCased, view } from "elm-native-js/types/Native/Constants.bs";
+  export var kebabCased;
+  export var view;
+}
